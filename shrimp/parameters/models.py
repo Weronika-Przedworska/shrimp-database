82581c5f0d2d989d4819303159e0a1c071ba3e04
Weronika-Przedworska <144631559+Weronika-Przedworska@users.noreply.github.com>
2025-12-13 18:49:20 -0500

    Added relationship between tank and measurements model

--- shrimp/parameters/models.py
+++ shrimp/parameters/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
-from django.db import models
+class Tank(models.Model):
+    size_liters = models.IntegerField() # volume of aquarium
+    brand = models.CharField(max_length=100) # aquarium brand name
+    water_type = models.CharField(max_length=50) #freshwater or saltwater or some secret third thing
+    def __str__(self):
+        return f"{self.brand} ({self.size_liters} liters)"
 
-class measurements(models.Model):
-  date=models.DateTimeField()
+
+class Measurement(models.Model):
+  date=models.DateTimeField(auto_now_add=True) #automatically gets date, better for accuracy
   ammonia = models.FloatField()
   nitrites = models.FloatField()
+  tank=models.ForeignKey(Tank, on_delete=models.CASCADE, related_name="measurements")
+
+  def __str__(self):
+        return f"{self.tank.brand} at ({self.date})"
+
 
 # Create your models here.
--- shrimp/parameters/views.py
+++ shrimp/parameters/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
-from django.template import loader
-
+ 
 
 # Create your views here.
 
+ 
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from .models import Measurement
+from .models import Tank
 
 def parameters(request):
-  template = loader.get_template('params.html')
-  return HttpResponse(template.render())
+    myparams = Measurement.objects.all()
+    return render(request, "allparams.html", {
+        "myparams": myparams
+    })
+
+
+
+#add one more view for adding values
