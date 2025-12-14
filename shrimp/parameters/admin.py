2850660919285071d7f89109b1ea1015d65df825
Weronika-Przedworska <144631559+Weronika-Przedworska@users.noreply.github.com>
2025-12-13 19:18:26 -0500

    Added models to admin page. Created rough draft of data display view

--- shrimp/parameters/admin.py
+++ shrimp/parameters/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Measurement
+from .models import Tank
+
 
 # Register your models here.
+
+# Register your models here.
+admin.site.register(Measurement)
+admin.site.register(Tank)
